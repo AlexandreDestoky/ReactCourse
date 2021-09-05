88ea7e19f113f820cc9c8deff0362e49b726f738
Destoky <adestoky@gmail.com>
2021-09-05 10:12:31 +0200

    Rendering Elements

--- react-docs-exercices/src/App.js
+++ react-docs-exercices/src/App.js
@@ -5,8 +5,11 @@ function App() {
   return (
     <div className="App">
       <h1>HELLO {nom}</h1>
+      <p>Il est {new Date().toLocaleTimeString()}.</p>
     </div>
   );
 }
+
+
 
 export default App;
