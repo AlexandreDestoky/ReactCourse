455c16d3f2e01653b2e77c9d1273fd930ad13deb
Destoky <adestoky@gmail.com>
2021-09-05 22:40:05 +0200

    Forms

--- react-docs-exercices/src/App.js
+++ react-docs-exercices/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import Liste from './Liste';
+// import Liste from './Liste';
+import Formulaire from './Formulaire';
 
 function App() {
   const tabPrenoms = ["Jean","Marc","Luc","Thomas"];
 
   return (
     <div className="App">
-      <Liste tab={tabPrenoms}/>
+      {/* <Liste tab={tabPrenoms}/> */}
+      <Formulaire/>
+
     </div>
   );
 }
