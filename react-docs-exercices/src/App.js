f8c0204c20e630c9ad33d8ee9c5229978a63a1bf
Destoky <adestoky@gmail.com>
2021-09-07 11:03:54 +0200

    Composition vs Inheritance

--- react-docs-exercices/src/App.js
+++ react-docs-exercices/src/App.js
@@ -1,23 +1,16 @@
 import "./App.css";
-import InputPoids from "./InputPoids";
 import { useState } from "react";
+import Footer from "./Footer";
 
 function App() {
-  const [weight, setWeight] = useState({
-    poids: "0",
-    format: "kg",
-  });
-  
-  const poidsKg = weight.format === "kg" ? weight.poids : (weight.poids / 2.2).toFixed(2);
-  const poidsLbs = weight.format === "lbs" ? weight.poids : (weight.poids * 2.2).toFixed(2);
-
-  const ajustePoidsKg = e => setWeight({ poids: e.target.value, format: "kg" });
-  const ajustePoidsLbs = e => setWeight({ poids: e.target.value, format: "lbs" });
 
   return (
     <div className="App">
-      <InputPoids placeholder="kg" format="kg" fonctionApp={ajustePoidsKg} poids={poidsKg} />
-      <InputPoids placeholder="lbs" format="lbs" fonctionApp={ajustePoidsLbs} poids={poidsLbs} />
+      <h1>APP</h1>
+      <Footer>
+        <h1>Le footer</h1>
+        <p>Ce texte va dans props.children</p>
+      </Footer>
     </div>
   );
 }
