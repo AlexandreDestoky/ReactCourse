47aaefedd24222a4f8ff8b09993608d25cd614ed
Destoky <adestoky@gmail.com>
2021-09-08 10:59:14 +0200

    Accessibility

--- react-docs-exercices/src/App.js
+++ react-docs-exercices/src/App.js
@@ -1,23 +1,14 @@
 import "./App.css";
 import { useState } from "react";
-import BlocProduit from "./BlocProduit";
 
 function App() {
-  const [dataProduit, setDataProduit] = useState([
-    { category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football" },
-    { category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball" },
-    { category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball" },
-    { category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch" },
-    { category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5" },
-    { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" },
-  ]);
 
   return (
     <div className="App">
       <h1>APP</h1>
-      <BlocProduit data={dataProduit}/>
+
     </div>
   );
 }
-
+//Explication optimisation accèsibilité pour personne avec handicap
 export default App;
